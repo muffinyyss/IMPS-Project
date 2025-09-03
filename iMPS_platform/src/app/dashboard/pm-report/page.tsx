568567eba6870a2756043f141fe704690fb79f3a
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-09-03 16:38:14 +0700

    upd: pm report add form pm report

--- iMPS_platform/src/app/dashboard/pm-report/page.tsx
+++ iMPS_platform/src/app/dashboard/pm-report/page.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 
-import SimpleDataTables from "./components/simple-data-table";
 import SearchDataTables from "./components/search-data-table";
 import StatisticsCards from "./components/statistics-cards";
 
--- iMPS_platform/src/app/dashboard/test-report/page.tsx
+++ iMPS_platform/src/app/dashboard/test-report/page.tsx
@@ -1,35 +1,29 @@
-  import React from "react";
-  import dynamic from "next/dynamic";
+import React from "react";
 
-  // @material-tailwind/react
-  import {
-    Card,
-    CardHeader,
-    CardBody,
-    Typography,
-  } from "@/components/MaterialTailwind";
+// @material-tailwind/react
+import { Card, CardHeader, CardBody, Typography } from "@/components/MaterialTailwind";
 
-  // @heroicons/react
-  import { GlobeAltIcon } from "@heroicons/react/24/outline";
+// @heroicons/react
+import { GlobeAltIcon } from "@heroicons/react/24/outline";
 
-  // components
-  import StatisticsCards from "./components/chargerInfo";
-  import SearchDataTables from "./components/search-data-table";
-  import ChargerInfo from "./components/chargerInfo";
+// components
+import SearchDataTables from "./components/search-data-table";
+import ChargerInfo from "./components/chargerInfo";
 
-
-
-  export default function DataTablesPage() {
-    return (
-      <div className="tw-mt-8 tw-mb-4">
-        {/* Sale by Country */}
-        <Card className="tw-mb-6 tw-border tw-border-blue-gray-100 tw-shadow-sm">
+export default function DataTablesPage() {
+  return (
+    <div className="tw-mt-8 tw-mb-4">
+      {/* Charger Info Card */}
+      <Card className="tw-mb-6 tw-border tw-border-blue-gray-100 tw-shadow-sm">
+        {/* Header */}
+        <div className="tw-flex tw-items-center tw-justify-between tw-px-4 tw-pt-4">
+          {/* left: icon + title */}
           <div className="tw-flex tw-items-center">
             <CardHeader
               floated={false}
               variant="gradient"
               color="gray"
-              className="tw-grid tw-h-16 tw-w-16 tw-place-items-center"
+              className="tw-grid tw-h-16 tw-w-16 tw-place-items-center tw-mr-3"
             >
               <GlobeAltIcon className="tw-h-7 tw-w-7 tw-text-white" />
             </CardHeader>
@@ -37,19 +31,22 @@
               Charger Info
             </Typography>
           </div>
-          <CardBody className="tw-grid tw-grid-cols-1 tw-items-center tw-justify-between tw-p-4 lg:tw-grid-cols-2">
-            <div className="tw-col-span-1">
-              {/* <Table /> */}
-              < ChargerInfo />
-            </div>
-            {/* <WorldMap /> */}
-          </CardBody>
-        </Card>
 
-        {/* <StatisticsCards /> */}
+          {/* right: ข้อมูลอุปกรณ์ชาร์จ */}
+          <Typography variant="small" className="tw-text-blue-gray-400 tw-mr-2">
+            ข้อมูลอุปกรณ์ชาร์จ
+          </Typography>
+        </div>
 
-        <SearchDataTables />
+        {/* Body */}
+        <CardBody className="tw-grid tw-grid-cols-1 tw-items-center tw-justify-between tw-p-4 lg:tw-grid-cols-2">
+          <div className="tw-col-span-1">
+            <ChargerInfo />
+          </div>
+        </CardBody>
+      </Card>
 
-      </div>
-    );
-  }
+      <SearchDataTables />
+    </div>
+  );
+}
