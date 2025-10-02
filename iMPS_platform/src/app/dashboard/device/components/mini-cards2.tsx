564c010433a8ff55c7e41137d09d04e897e96898
muffinyyss <152370452+muffinyyss@users.noreply.github.com>
2025-10-02 16:50:38 +0700

    upd: device page

---
+++ iMPS_platform/src/app/dashboard/device/components/mini-cards2.tsx
@@ -0,0 +1,22 @@
+"use client";
+import React from "react";
+
+// widget
+import { MiniCard } from "@/widgets/cards";
+
+// data
+import { deviceCardData2 } from "@/app/dashboard/device/data";
+
+type Props = {};
+
+export default function MiniCards({}: Props) {
+  return (
+    <div className="tw-col-span-2 tw-grid tw-w-full tw-items-center tw-gap-6 md:tw-col-span-2">
+      {deviceCardData2.map((props) => (
+        <MiniCard key={props.title} {...props} />
+      ))}
+    </div>
+
+    
+  );
+}
