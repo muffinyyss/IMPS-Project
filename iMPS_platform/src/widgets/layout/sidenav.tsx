f2c4eb4a7444476867e68ef9c102e52f5c32f9e5
ThasawanSnongphan <thasawan.tin@gmail.com>
2026-09-11 11:25:05 +0700

    Update sidenav.tsx

--- iMPS_platform/src/widgets/layout/sidenav.tsx
+++ iMPS_platform/src/widgets/layout/sidenav.tsx
@@ -425,7 +425,10 @@ export default function Sidenav({ }: PropTypes) {
                   </ListItem>
 
                   <AccordionBody className="!tw-py-1 tw-text-inherit">
-                    <List className={`!tw-p-0 tw-text-inherit ${name === "EV Stations" ? "!tw-pl-4" : ""}`}>
+                    <List className={`!tw-p-0 tw-text-inherit ${name === "EV Stations"
+                      ? `!tw-ml-2 !tw-pl-4 tw-border-l ${sidenavType === "dark" ? "tw-border-white/20" : "tw-border-gray-300"}`
+                      : ""
+                      }`}>
                       {pages.map((page, idx) =>
                         page.pages ? (
                           <Accordion
